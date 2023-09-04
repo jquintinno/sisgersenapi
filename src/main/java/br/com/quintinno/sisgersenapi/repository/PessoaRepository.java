a81042f037855edac83f891c774b20bb8a6f07e7
Quintinno Digital <jquintinno@gmail.com>
2023-09-04 20:45:51 -0300

    Feature: Implementar operações de persistência com arquivo XML externo

--- src/main/java/br/com/quintinno/sisgersenapi/repository/PessoaRepository.java
+++ src/main/java/br/com/quintinno/sisgersenapi/repository/PessoaRepository.java
@@ -16,12 +16,8 @@ public class PessoaRepository {
     private PessoaDAO pessoaDAO;
 
     public PessoaDomain create(PessoaDomain pessoaDomain) {
-        String sql = pessoaDAO.getQuery();
-        for (String query : sql.split(PessoaDAO.QUERY_0001)) {
-            if (query.trim().startsWith("\n")) {
-                this.jdbcTemplate.update(query, pessoaDomain.getCategoria(), pessoaDomain.getNome());
-            }
-        }
+        String query = pessoaDAO.recuperarQuery("CREATE");
+        this.jdbcTemplate.update(query, pessoaDomain.getCategoria(), pessoaDomain.getNome());
         return pessoaDomain;
     }
 
--- src/main/java/br/com/quintinno/sisgersenapi/sql/PessoaDAO.java
+++ src/main/java/br/com/quintinno/sisgersenapi/sql/PessoaDAO.java
@@ -1,30 +1,53 @@
 package br.com.quintinno.sisgersenapi.sql;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import java.io.BufferedReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class PessoaDAO {
 
-    public static final String QUERY_0001 = "-- QUERY_0001";
-    public static final String QUERY_0002 = "-- QUERY_0002";
+    private final Map<String, String> queries;
 
-    private final String query;
+    public PessoaDAO() throws Exception {
+        this.queries = new HashMap<>();
+        getQuery();
+    }
 
-    public PessoaDAO(@Value("classpath:pessoa_query.sql") Resource resource) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
-            this.query = reader.lines().collect(Collectors.joining("\n"));
+    private void getQuery() throws Exception {
+        try {
+            PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
+            Resource resource = pathMatchingResourcePatternResolver.getResource("classpath:pessoa_query.xml");
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(resource.getInputStream());
+
+            NodeList queryNodes = document.getElementsByTagName("query");
+
+            for (int i = 0; i < queryNodes.getLength(); i++) {
+                Node queryNode = queryNodes.item(i);
+                if (queryNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element queryElement = (Element) queryNode;
+                    queries.put(queryElement.getAttribute("nome"), queryElement.getTextContent().trim());
+                }
+            }
+        } catch (IOException e) {
+            throw new Exception("Erro ao carregar o arquivo XML!", e);
         }
     }
 
-    public String getQuery() {
-        return query;
+    public String recuperarQuery(String name) {
+        return queries.get(name);
     }
 
 }
